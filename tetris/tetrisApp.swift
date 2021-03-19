b3a63d586c450165d2dedc59b89fe7da02760cf2
Rick Brown <ricky.brown.00@gmail.com>
2021-03-19 23:40:20 +0000

    Initial Commit

---
+++ tetris/tetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  tetrisApp.swift
+//  tetris
+//
+//  Created by Rick Brown on 19/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct tetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
